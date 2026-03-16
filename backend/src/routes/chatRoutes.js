5e012da9353894e392e79d033cac4b853d62ee18
shloksharmawork <shloksharmawork@gmail.com>
2026-03-17 01:51:37 +0530

    Fix: Correct auth middleware import in chatRoutes.js

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getMessages, sendMessage } = require('../controllers/chatController');
-const auth = require('../middlewares/auth');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.use(auth);
+router.use(authMiddleware);
 
 router.get('/:friendId', getMessages);
 router.post('/', sendMessage);
